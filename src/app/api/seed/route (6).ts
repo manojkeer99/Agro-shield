8c101780c529c43be843e29997f955ec9f3f1234
Manoj keer <keermanoj99@gmail.com>
2026-09-10 19:14:56 +0530

    Update route (6).ts

--- src/app/api/seed/route (6).ts
+++ src/app/api/seed/route (6).ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { seedDatabaseIfEmpty } from "@/lib/seed";
+import  seedDatabaseIfEmpty  from "@/lib/seed";
 
 export async function POST(req: NextRequest) {
   try {
